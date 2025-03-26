fa58ef1baebb623075610cbbb1fd7865cfbcc9c0
AbcZoe <5B1G0008@stust.edu.tw>
2025-03-26 14:23:14 +0800

    在js檔裡增加了等級選擇功能。在html裡加了開始遊戲的按鈕，並對它加了點擊事件

--- waterpuzzle.js
+++ waterpuzzle.js
@@ -7,12 +7,17 @@ document.addEventListener('DOMContentLoaded',()=>{
     const tubes=[];
     let levelCount=1;
 
+    function chooseLevel(Level){
+        levelCount=Level;
+        document.getElementById('level-count').textContent=levelCount;
+    }
     labelSelect.addEventListener('change',(event)=>{
-        const selectedLevel=parseInt(event.target.value,10);
         //parseInt(字串, 幾進位制);將一個字符串解析並轉換為整數
+        const selectedLevel=parseInt(event.target.value,10);
+        chooseLevel(selectedLevel);
     });
 
     playerButton.addEventListener('click',()=>{
-
+        alert('開始玩遊戲!')
     });
 });
